339757e6994fe10a41baa7e3e12e75b3c928c5d5
tomato-gits <lauren.moore432@gmail.com>
2024-08-23 15:13:04 -0700

    adds custom css to docs theme and adjusts logo width

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,6 +81,7 @@ extlinks = {
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_css_files = ['custom.css']
 
 
 # -- Options for OGP social preview ------------------------------------------
