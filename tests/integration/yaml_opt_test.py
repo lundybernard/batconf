339702f182936070e76e999c49d47a5117056e97
LundyBernard <lundy.bernard@gmail.com>
2025-03-31 12:36:50 -0700

    add config.yaml file to opt yaml test

--- tests/integration/yaml_opt_test.py
+++ tests/integration/yaml_opt_test.py
@@ -1,5 +1,8 @@
 from unittest import TestCase, skipIf
 
+from os import path
+
+from batconf.sources.yaml import YamlConfig
 from batconf.sources.tests.yaml_test import (
     EXAMPLE_CONFIG_DICT,
     EXAMPLE_CONFIG_YAML,
@@ -34,14 +37,14 @@ class SourcesYamlUnittestStaticValuesTests(TestCase):
 
 
 @skipIf(not _PYYAML_INSTALLED, 'optional pyyaml module not installed')
-class SourcesFileUnittestStaticValuesTests(TestCase):
-    def test_EXAMPLE_CONFIG_DICT(t):
-        loaded_dict = yaml.load(EXAMPLE_CONFIG_YAML, Loader=yaml.BaseLoader)
-        t.assertDictEqual(EXAMPLE_CONFIG_DICT, loaded_dict)
+class YamlFileIntegrationTests(TestCase):
+    def test_yaml_file_source_defaults(t):
+        # Get the OS-agnostic absolute path to the test config.yaml file
+        this_dir = path.dirname(path.realpath(__file__))
+        yml_config_file_name = path.join(this_dir, 'data', 'config.yaml')
+        yc = YamlConfig(config_file_name=yml_config_file_name)
 
-    def test_EXAMPLE_CONFIG_WITHOUT_ENVIRONMENTS(t):
-        loaded_envless_dict = yaml.load(
-            EXAMPLE_CONFIG_WITHOUT_ENVIRONMENTS,
-            Loader=yaml.BaseLoader,
+        t.assertEqual(
+            'Config.yaml: test.project.submodule.sub.key1',
+            yc.get('project.submodule.sub.key1'),
         )
-        t.assertDictEqual(EXAMPLE_CONFIG_WITHOUT_ENV_DICT, loaded_envless_dict)
