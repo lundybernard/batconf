476a779859421124f8f3f503c49e6e99b700d91d
LundyBernard <lundy.bernard@gmail.com>
2025-04-12 17:17:41 -0700

    lint

--- batconf/sources/tests/file_test.py
+++ batconf/sources/tests/file_test.py
@@ -15,7 +15,7 @@ from ..file import (
 SRC = 'batconf.sources.file'
 
 
-EXAMPLE_CONFIG_YAML = '''
+EXAMPLE_CONFIG_YAML = """
 default: example
 
 example:
@@ -29,7 +29,7 @@ alt:
     bat:
         module:
             key: alt_value
-'''
+"""
 
 
 EXAMPLE_CONFIG_DICT: dict = {
@@ -39,8 +39,10 @@ EXAMPLE_CONFIG_DICT: dict = {
             'key': 'value',
             'remote_host': {
                 'api_key': 'example_api_key',
-                'url': 'https://api-example.host.io/'
-    },},},
+                'url': 'https://api-example.host.io/',
+            },
+        },
+    },
     'alt': {'bat': {'module': {'key': 'alt_value'}}},
 }
 
@@ -50,7 +52,10 @@ class TestFileConfig(TestCase):
     _load_yaml_file: Mock
 
     def setUp(t):
-        patches = ['Path', '_load_yaml_file',]
+        patches = [
+            'Path',
+            '_load_yaml_file',
+        ]
         for target in patches:
             patcher = patch(f'{SRC}.{target}', autospec=True)
             setattr(t, target, patcher.start())
@@ -70,13 +75,15 @@ class TestFileConfig(TestCase):
         with t.subTest('single key'):
             t.assertEqual(
                 conf.get('bat.key'),
-                EXAMPLE_CONFIG_DICT['example']['bat']['key']
+                EXAMPLE_CONFIG_DICT['example']['bat']['key'],
             )
 
         with t.subTest('key from module'):
             t.assertEqual(
                 conf.get('api_key', module='bat.remote_host'),
-                EXAMPLE_CONFIG_DICT['example']['bat']['remote_host']['api_key']
+                EXAMPLE_CONFIG_DICT['example']['bat']['remote_host'][
+                    'api_key'
+                ],
             )
         with t.subTest('missing item'):
             t.assertEqual(conf.get('_sir_not_appearing_in_this_film'), None)
@@ -91,30 +98,21 @@ class TestFileConfig(TestCase):
         )
 
     def test_loads_given_config_file(t):
-        _ = FileConfig(
-            './test_example.config.yaml',
-            config_env='example'
-        )
+        _ = FileConfig('./test_example.config.yaml', config_env='example')
         t._load_yaml_file.assert_called_with(t.Path.return_value)
         t.Path.assert_called_with('./test_example.config.yaml')
 
-
     def test_config_env_argument(t):
         t._load_yaml_file.return_value = EXAMPLE_CONFIG_DICT
-        config_file = FileConfig(
-            './example.config.yaml', config_env='alt'
-        )
+        config_file = FileConfig('./example.config.yaml', config_env='alt')
         t._load_yaml_file.assert_called_with(t.Path.return_value)
         t.Path.assert_called_with('./example.config.yaml')
-        t.assertEqual(
-            config_file.get('key', module='bat.module'), 'alt_value'
-        )
+        t.assertEqual(config_file.get('key', module='bat.module'), 'alt_value')
 
     @patch.dict(f'{SRC}.os.environ', {}, clear=True)
     @patch(f'{SRC}.log', autospec=True)
     def test_missing_file(t, log):
-        '''No file found
-        '''
+        """No file found"""
         t.Path.return_value.is_file.return_value = False
         conf = FileConfig()
         log.warning.assert_called_with(_missing_config_warning)
@@ -125,13 +123,12 @@ class TestFileConfig(TestCase):
 
         with t.subTest('dot notation key path'):
             t.assertEqual(
-                config_file['bat.remote_host.api_key'],
-                'example_api_key'
+                config_file['bat.remote_host.api_key'], 'example_api_key'
             )
         with t.subTest('__getitem__ chain'):
             t.assertEqual(
                 config_file['bat']['remote_host']['url'],
-                'https://api-example.host.io/'
+                'https://api-example.host.io/',
             )
 
     def test_keys(t):
@@ -143,7 +140,9 @@ class Test_load_config_file(TestCase):
     _load_yaml_file: Mock
 
     def setUp(t):
-        patches = ['_load_yaml_file', ]
+        patches = [
+            '_load_yaml_file',
+        ]
         for target in patches:
             patcher = patch(f'{SRC}.{target}', autospec=True)
             setattr(t, target, patcher.start())
@@ -168,24 +167,16 @@ class Test_load_config_file(TestCase):
 
         t.assertEqual(default_env, conf['example'])
 
-    @patch.dict(
-        f'{SRC}.os.environ', {'BAT_CONFIG_FILE': 'env.config.yaml'}
-    )
+    @patch.dict(f'{SRC}.os.environ', {'BAT_CONFIG_FILE': 'env.config.yaml'})
     def test_config_file_env_variable(t):
         conf = load_config_file()
 
-        t._load_yaml_file.assert_called_with(
-            file_path=Path('env.config.yaml')
-        )
+        t._load_yaml_file.assert_called_with(file_path=Path('env.config.yaml'))
 
         example_env = conf['example']['bat']
+        t.assertEqual(example_env['remote_host']['api_key'], 'example_api_key')
         t.assertEqual(
-            example_env['remote_host']['api_key'],
-            'example_api_key'
-        )
-        t.assertEqual(
-            example_env['remote_host']['url'],
-            'https://api-example.host.io/'
+            example_env['remote_host']['url'], 'https://api-example.host.io/'
         )
 
     @patch.dict(f'{SRC}.os.environ', {}, clear=True)
@@ -200,21 +191,16 @@ class Test_load_config_file(TestCase):
             file_path=Path(os.getcwd() + '/config.yaml')
         )
         example_env = CONF['example']['bat']
+        t.assertEqual(example_env['remote_host']['api_key'], 'example_api_key')
         t.assertEqual(
-            example_env['remote_host']['api_key'],
-            'example_api_key'
-        )
-        t.assertEqual(
-            example_env['remote_host']['url'],
-            'https://api-example.host.io/'
+            example_env['remote_host']['url'], 'https://api-example.host.io/'
         )
 
     @patch.dict(f'{SRC}.os.environ', {}, clear=True)
     @patch(f'{SRC}.log', autospec=True)
     @patch(f'{SRC}.Path.is_file', autospec=True)
     def test_config_missing_file(t, path_is_file, log):
-        '''PROJECT_CONFIG is not set
-        '''
+        """PROJECT_CONFIG is not set"""
         path_is_file.return_value = False
         conf = load_config_file()
         log.warning.assert_called_with(_missing_config_warning)
--- tests/integration/yaml_opt_test.py
+++ tests/integration/yaml_opt_test.py
@@ -16,15 +16,13 @@ except ImportError:
 
 @skipIf(not _PYYAML_INSTALLED, 'optional pyyaml module not installed')
 class SourcesYamlUnittestStaticValuesTests(TestCase):
-    '''If pyyaml is installed,
+    """If pyyaml is installed,
     use it to validate that our assumptions
     about how yaml strings will be converted to dictionaries
-    '''
+    """
+
     def test_EXAMPLE_CONFIG_DICT(t):
-        loaded_dict = yaml.load(
-            EXAMPLE_CONFIG_YAML,
-            Loader=yaml.BaseLoader
-        )
+        loaded_dict = yaml.load(EXAMPLE_CONFIG_YAML, Loader=yaml.BaseLoader)
         t.assertDictEqual(EXAMPLE_CONFIG_DICT, loaded_dict)
 
     def test_EXAMPLE_CONFIG_WITHOUT_ENVIRONMENTS(t):
@@ -32,19 +30,13 @@ class SourcesYamlUnittestStaticValuesTests(TestCase):
             EXAMPLE_CONFIG_WITHOUT_ENVIRONMENTS,
             Loader=yaml.BaseLoader,
         )
-        t.assertDictEqual(
-            EXAMPLE_CONFIG_WITHOUT_ENV_DICT,
-            loaded_envless_dict
-        )
+        t.assertDictEqual(EXAMPLE_CONFIG_WITHOUT_ENV_DICT, loaded_envless_dict)
 
 
 @skipIf(not _PYYAML_INSTALLED, 'optional pyyaml module not installed')
 class SourcesFileUnittestStaticValuesTests(TestCase):
     def test_EXAMPLE_CONFIG_DICT(t):
-        loaded_dict = yaml.load(
-            EXAMPLE_CONFIG_YAML,
-            Loader=yaml.BaseLoader
-        )
+        loaded_dict = yaml.load(EXAMPLE_CONFIG_YAML, Loader=yaml.BaseLoader)
         t.assertDictEqual(EXAMPLE_CONFIG_DICT, loaded_dict)
 
     def test_EXAMPLE_CONFIG_WITHOUT_ENVIRONMENTS(t):
@@ -52,7 +44,4 @@ class SourcesFileUnittestStaticValuesTests(TestCase):
             EXAMPLE_CONFIG_WITHOUT_ENVIRONMENTS,
             Loader=yaml.BaseLoader,
         )
-        t.assertDictEqual(
-            EXAMPLE_CONFIG_WITHOUT_ENV_DICT,
-            loaded_envless_dict
-        )
+        t.assertDictEqual(EXAMPLE_CONFIG_WITHOUT_ENV_DICT, loaded_envless_dict)
