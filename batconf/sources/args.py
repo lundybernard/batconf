c0763a0cab2b0e40800d637188efb31dc3c66694
LundyBernard <lundy.bernard@gmail.com>
2025-05-07 14:37:05 -0700

    update example to use new argparse Namespace source

--- batconf/sources/args.py
+++ batconf/sources/args.py
@@ -4,6 +4,31 @@ from ..source import SourceInterface, OpStr
 
 
 class CliArgsConfig(SourceInterface):
+    """Legacy argparse.Namespace configuration source.
+
+    Using this source, the key value will overwrite every
+    option where the final key in its path matches.
+    project.key1, project.thing1.key1, project.thing2.key1, etc.
+    would all be set to "crash".
+
+    ex:
+    ```
+    > bat print_config key1=crash key2=override`
+    project <class 'project.cfg.ProjectConfig'>:
+        |- submodule <class 'project.submodule.SubmoduleConfig'>:
+        |    |- sub <class 'project.submodule.sub.MyClient.Config'>:
+        |    |    |- key1: "crash"
+        |    |    |- key2: "override"
+        |- clients <class 'project.cfg.ClientsSchema'>:
+        |    |- key1: "crash"
+    ```
+
+    This is often sufficient for smaller projects, where the name collisions
+    do not cause a problem,
+    and allows for simpler argparser setup where you do not need to specify
+    a 'dest=' parameter for arguments.
+    """
+
     def __init__(self, args: Namespace) -> None:
         self._data = args
 
